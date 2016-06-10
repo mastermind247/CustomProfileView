231c8d17b620e7e240a9209afee162010e380570
Parth <parth.adroja@solutionanalysts.com>
2016-06-10 18:29:49 +0530

    Removed pods

--- IBdesignDemo/Pods/Target Support Files/Pods-IBdesignDemo/Pods-IBdesignDemo-frameworks.sh
+++ IBdesignDemo/Pods/Target Support Files/Pods-IBdesignDemo/Pods-IBdesignDemo-frameworks.sh
@@ -84,18 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireNetworkActivityIndicator/AlamofireNetworkActivityIndicator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Koloda/Koloda.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireImage/AlamofireImage.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/AlamofireNetworkActivityIndicator/AlamofireNetworkActivityIndicator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Kingfisher/Kingfisher.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Koloda/Koloda.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/pop/pop.framework"
 fi
